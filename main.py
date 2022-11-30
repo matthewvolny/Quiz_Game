44f866446b3849fcdd98e8050a754be8b62ff641
matthewvolny <matthew.volny@gmail.com>
2022-11-30 11:44:02 -0500

    game functionally complete

--- main.py
+++ main.py
@@ -8,20 +8,29 @@ for question in question_data:
     newQuestion = Question(question['text'], question['answer'])
     question_bank.append(newQuestion)
 
-game_running = True
-
-while game_running:
-
+def game():
+    
+    continue_game = True
     quiz_brain = QuizBrain(question_bank)
-    question_number = quiz_brain.question_number
-    current_question = quiz_brain.get_question()
-    user_answer = input(f"Q.{question_number}: {current_question.text} (True/False)?: ")
-    
-    if user_answer == current_question.answer:
-        print("Correct! How about another question?")
-        quiz_brain.increment_question()
-    
 
-    game_running = False
+    while continue_game:
+
+        question_number = quiz_brain.question_number
+        current_question = quiz_brain.get_question()
+        user_answer = input(f"Q.{question_number}: {current_question.text} (True/False)?: ")
+        
+        if user_answer == current_question.answer:
+            print("Great job! Here is another one.")
+            quiz_brain.increment_question()
+        else:
+            print("Sorry that is incorrect.")
+            try_again = input("Would you like to play again (True/False)?: ")
+            if try_again == True:
+                game()
+            else:    
+                print("Goodbye.")
+                continue_game = False
+
+game()
     
 
